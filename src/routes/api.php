62e5043c599a7b94d560ed0faa4074f02123e7e6
Abdul <abdul@uitouxsolutions.com>
2020-05-02 07:11:26 +0530

    Update api.php

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,7 +2,7 @@
 Route::group(['namespace' => 'Abs\GigoPkg\Api', 'middleware' => ['api']], function () {
 	Route::group(['prefix' => 'gigo-pkg'], function () {
 		Route::group(['middleware' => ['auth:api']], function () {
-			// Route::get('taxes/get', 'TaxController@getTaxes');
+			Route::post('save-vehicle-gate-in-entry', 'VehicleGatePassController@saveVehicleGateInEntry');
 		});
 	});
 });
