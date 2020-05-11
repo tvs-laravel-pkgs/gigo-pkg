8307fcf32823684616605e036219b6ea32ba44d9
Vijay-UITOUX <52068840+Vijay-UITOUX@users.noreply.github.com>
2020-05-11 17:01:14 +0530

    vehicle inspection item api

--- src/models/JobOrder.php
+++ src/models/JobOrder.php
@@ -35,8 +35,13 @@ class JobOrder extends Model {
 	public function customerVoice() {
 		return $this->belongsToMany('App\CustomerVoice', 'job_order_customer_voice', 'job_order_id', 'customer_voice_id');
 	}
+
+	public function jobOrderVehicleInspectionItem() {
+		return $this->belongsToMany('App\VehicleInspectionItem', 'job_order_vehicle_inspection_item', 'job_order_id', 'vehicle_inspection_item_id')->withPivot(['status_id']);
+	}
+
 	public function vehicleInventoryItem() {
-		return $this->belongsToMany('App\VehicleInventoryItem', 'job_order_vehicle_inventory_item', 'job_order_id', 'vehicle_inventory_item_id')->withPivot(['is_available','remarks']);
+		return $this->belongsToMany('App\VehicleInventoryItem', 'job_order_vehicle_inventory_item', 'job_order_id', 'vehicle_inventory_item_id')->withPivot(['is_available', 'remarks']);
 	}
 
 	public static function createFromObject($record_data) {
