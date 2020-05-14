784694b8b6f8ac978d0fe9dbf71c11a39e80dd62
saravana-uitoux <saravanan@uitouxsolutions.com>
2020-05-14 10:25:44 +0530

    job card get list inprogress

--- src/controllers/api/JobCardController.php
+++ src/controllers/api/JobCardController.php
@@ -14,6 +14,68 @@ use Validator;
 class JobCardController extends Controller {
 	public $successStatus = 200;
 
+	public function getJobCardList(Request $request) {
+		try {
+			$validator = Validator::make($request->all(), [
+				'employee_id' => [
+					'required',
+					'exists:employees,id',
+					'integer',
+				],
+			]);
+			if ($validator->fails()) {
+				return response()->json([
+					'success' => false,
+					'errors' => $validator->errors()->all(),
+				]);
+			}
+
+			$jobcard_ids = [];
+			$jobcards = Jobcard::
+				where('jobcards.company_id', Auth::user()->company_id)
+				->get();
+			foreach ($jobcards as $key => $jobcard) {
+				if ($jobcard->status_id == 8120) {
+					//Gate In Completed
+					$jobcard_ids[] = $jobcard->id;
+				} else {
+// Others
+					if ($jobcard->floor_adviser_id == $request->employee_id) {
+						$jobcard_ids[] = $jobcard->id;
+					}
+				}
+			}
+
+			$job_card_list = Jobcard::select('jobcards.*')
+				->with([
+					'vehicleDetail',
+					'vehicleDetail.vehicleOwner',
+					'vehicleDetail.vehicleOwner.CustomerDetail',
+				])
+				->leftJoin('vehicles', 'jobcards.vehicle_id', 'vehicles.id')
+				->leftJoin('vehicle_owners', 'vehicles.id', 'vehicle_owners.vehicle_id')
+				->leftJoin('customers', 'vehicle_owners.customer_id', 'customers.id')
+				->whereIn('jobcards.id', $jobcard_ids)
+				->where(function ($query) use ($request) {
+					if (isset($request->search_key)) {
+						$query->where('vehicles.registration_number', 'LIKE', '%' . $request->search_key . '%')
+							->orWhere('customers.name', 'LIKE', '%' . $request->search_key . '%');
+					}
+				})
+				->get();
+
+			return response()->json([
+				'success' => true,
+				'vehicle_inward_list' => $vehicle_inward_list,
+			]);
+		} catch (Exception $e) {
+			return response()->json([
+				'success' => false,
+				'errors' => ['Exception Error' => $e->getMessage()],
+			]);
+		}
+	}
+
 	public function saveJobCard(Request $request) {
 		//dd($request->all());
 		try {
