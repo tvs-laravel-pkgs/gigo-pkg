d3bc4a4df71b93308391bbc2cfe7893b6bd091dd
Abdul <abdul@uitouxsolutions.com>
2020-06-10 03:58:25 +0530

    wc di

--- src/public/design-input/warranty-job-order-request/controller.js
+++ src/public/design-input/warranty-job-order-request/controller.js
@@ -1,6 +1,6 @@
-app.directive('inwardViewTabs', function() {
+app.directive('warrantyJobOrderRequestFormTabs', function() {
     return {
-        templateUrl: inward_view_tabs_template_url,
+        templateUrl: warrantyJobOrderRequestFormTabs,
         controller: function() {}
     }
 });
@@ -48,6 +48,114 @@ app.component('warrantyJobOrderRequestPprForm', {
         $element.find('input').on('keydown', function(ev) {
             ev.stopPropagation();
         });
+        $scope.extras = {};
+        $scope.extras.service_types = [{
+                id: 1,
+                name: 'Pre Delivery Inspection',
+            },
+            {
+                id: 2,
+                name: '2nd Free Service',
+            },
+            {
+                id: 3,
+                name: '3rd Free Service',
+            },
+        ];
+
+        $scope.extras.complaints = [{
+                id: 1,
+                code: '01A1',
+                name: 'CYL BLOCK CASTING DEFECT - OIL LEAK',
+            },
+            {
+                id: 2,
+                code: '01A2',
+                name: 'CYL BLOCK CASTING DEFECT- COOLANT LEAK',
+            },
+            {
+                id: 3,
+                code: '01A3',
+                name: 'CYL HEAD CASTING DEFECT- OIL LEAK',
+            },
+            {
+                id: 4,
+                code: '01B9',
+                name: 'Block Machining defect',
+            },
+            {
+                id: 5,
+                code: '01C3',
+                name: 'BLOCK CRACK',
+            },
+            {
+                id: 6,
+                code: '01C4',
+                name: 'NEP-OVERHEAD CAMSHAFT TRIGGER WHEEL',
+            },
+            {
+                id: 7,
+                code: '01C5',
+                name: 'NEP-OVERHEAD CAMSHAFT GEAR',
+            },
+        ];
+
+        $scope.searchCompaints = function(query) {
+            var results = query ? $scope.extras.complaints.filter(createFilterFor(query)) : $scope.extras.complaints,
+                deferred;
+            return results;
+        }
+
+        $scope.extras.faults = [{
+                id: 1,
+                code: 'AA',
+                name: 'ADRIFT/LOOSE',
+            },
+            {
+                id: 2,
+                code: 'AB',
+                name: 'AIR BUBBLE',
+            },
+            {
+                id: 3,
+                code: 'AC',
+                name: 'AIR LEAK',
+            },
+            {
+                id: 4,
+                code: 'AD',
+                name: 'AIR LOCK',
+            },
+            {
+                id: 5,
+                code: 'AE',
+                name: 'BLOCK CRACK',
+            },
+            {
+                id: 6,
+                code: 'AF',
+                name: 'BLOCKED/CLOGGED',
+            },
+            {
+                id: 7,
+                code: 'AG',
+                name: 'BROKEN/BURST/CRACKED',
+            },
+        ];
+
+        $scope.searchFaults = function(query) {
+            var results = query ? $scope.extras.faults.filter(createFilterFor(query)) : $scope.extras.faults,
+                deferred;
+            return results;
+        }
+
+        function createFilterFor(query) {
+            var lowercaseQuery = query.toLowerCase();
+            return function filterFn(item) {
+                return (item.code.indexOf(lowercaseQuery) === 0);
+            };
+
+        }
         // $scope.gate_logs = res.gate_logs;
         $rootScope.loading = false;
     }
--- src/views/design-input-setup.blade.php
+++ src/views/design-input-setup.blade.php
@@ -12,6 +12,9 @@
     var designInputWarrantyJobOrderRequest = "{{asset($gigo_pkg_prefix.'/public/design-input/warranty-job-order-request/list.html')}}";
     var warrantyJobOrderRequestPPRForm = "{{asset($gigo_pkg_prefix.'/public/design-input/warranty-job-order-request/ppr-form.html')}}";
     var warrantyJobOrderRequestEstimateForm = "{{asset($gigo_pkg_prefix.'/public/design-input/warranty-job-order-request/estimate-form.html')}}";
+
+	//partials
+    var warrantyJobOrderRequestFormTabs = "{{asset($gigo_pkg_prefix.'/public/design-input/warranty-job-order-request/partials/warranty-job-order-request-form-tabs.html')}}";	
 </script>
 
 <script type="text/javascript" src="{{asset($gigo_pkg_prefix.'/public/design-input/warranty-job-order-request/controller.js')}}"></script>
