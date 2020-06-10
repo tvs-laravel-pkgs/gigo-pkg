22eb503ee8f56253ef59d3ae0defc3941cc4e837
Abdul <abdul@uitouxsolutions.com>
2020-06-10 16:49:57 +0530

    complaint and fault migrations

---
+++ src/database/migrations/2020_06_10_153722_complaints_c.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ComplaintsC extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+
+		if (!Schema::hasTable('complaints')) {
+			Schema::create('complaints', function (Blueprint $table) {
+
+				$table->increments('id');
+				$table->unsignedInteger('company_id');
+				$table->string('code', 32);
+				$table->string('name', 191);
+				$table->unsignedInteger('group_id');
+				$table->unsignedInteger('hours')->nullable();
+				$table->unsignedInteger('kms')->nullable();
+				$table->unsignedMediumInteger('months')->nullable();
+				$table->unsignedInteger("created_by_id")->nullable();
+				$table->unsignedInteger("updated_by_id")->nullable();
+				$table->unsignedInteger("deleted_by_id")->nullable();
+				$table->timestamps();
+				$table->softDeletes();
+
+				$table->foreign("created_by_id")->references("id")->on("users")->onDelete("SET NULL")->onUpdate("cascade");
+				$table->foreign("updated_by_id")->references("id")->on("users")->onDelete("SET NULL")->onUpdate("cascade");
+				$table->foreign("deleted_by_id")->references("id")->on("users")->onDelete("SET NULL")->onUpdate("cascade");
+
+				$table->unique(["company_id", "group_id", "code"]);
+				$table->unique(["company_id", "group_id", "name"]);
+
+			});
+		}
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('complaints');
+	}
+}
