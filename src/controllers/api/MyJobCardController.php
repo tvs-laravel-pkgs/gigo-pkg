f0c9102d4a0dcfc78695f0588b1f9e9b848528c7
Vijay-UITOUX <52068840+Vijay-UITOUX@users.noreply.github.com>
2020-06-19 10:23:38 +0530

    my job card list

--- src/controllers/api/MyJobCardController.php
+++ src/controllers/api/MyJobCardController.php
@@ -2,8 +2,9 @@
 
 namespace Abs\GigoPkg\Api;
 
-use App\Employee;
+use Abs\GigoPkg\JobCard;
 use App\Http\Controllers\Controller;
+use App\User;
 use DB;
 use Illuminate\Http\Request;
 use Validator;
@@ -15,9 +16,9 @@ class MyJobCardController extends Controller {
 		// dd($request->all());
 		try {
 			$validator = Validator::make($request->all(), [
-				'employee_id' => [
+				'user_id' => [
 					'required',
-					'exists:employees,id',
+					'exists:users,id',
 					'integer',
 				],
 			]);
@@ -27,34 +28,38 @@ class MyJobCardController extends Controller {
 					'errors' => $validator->errors()->all(),
 				]);
 			}
-			
-			$user_details = Employee::
-			with(['user',
-				'outlet',
-				'outlet.state'])->find($request->employee_id);
 
-			$my_job_card_list = Employee::select([
+			$user_details = User::with([
+				'employee',
+				'employee.outlet',
+				'employee.outlet.state',
+			])
+				->find($request->user_id);
+
+			$my_job_card_list = JobCard::select([
 				'job_cards.id',
 				'job_cards.job_card_number as jc_number',
 				'vehicles.registration_number',
 				DB::raw('COUNT(job_order_repair_orders.id) as no_of_ROTs'),
 				'configs.name as status',
-				DB::raw('DATE_FORMAT(gate_logs.gate_in_date,"%d/%m/%Y") as date'),
-				DB::raw('DATE_FORMAT(gate_logs.gate_in_date,"%h:%i %p") as time'),
-				'models.model_number','gate_logs.number as gatelog_number','users.name as user_name',
+				DB::raw('DATE_FORMAT(job_cards.created_at,"%d/%m/%Y") as date'),
+				DB::raw('DATE_FORMAT(job_cards.created_at,"%h:%i %p") as time'),
+				'models.model_number',
+				'customers.name as customer_name',
 			])
-				->join('users', 'users.entity_id', 'employees.id')
-				->join('repair_order_mechanics', 'repair_order_mechanics.mechanic_id', 'users.id')
-				->join('job_order_repair_orders', 'job_order_repair_orders.id', 'repair_order_mechanics.job_order_repair_order_id')
-				->join('job_orders', 'job_orders.id', 'job_order_repair_orders.job_order_id')
-				->join('gate_logs', 'gate_logs.job_order_id', 'job_orders.id')
+				->join('job_orders', 'job_orders.id', 'job_cards.job_order_id')
+				->join('job_order_repair_orders', 'job_order_repair_orders.job_order_id', 'job_orders.id')
+				->join('repair_order_mechanics', 'repair_order_mechanics.job_order_repair_order_id', 'job_order_repair_orders.id')
 				->join('vehicles', 'vehicles.id', 'job_orders.vehicle_id')
-				->leftJoin('models', 'models.id', 'vehicles.model_id')
-				->join('job_cards', 'job_cards.job_order_id', 'job_orders.id')
+				->join('vehicle_owners', function ($join) {
+					$join->on('vehicle_owners.vehicle_id', 'job_orders.vehicle_id')
+						->whereRaw('vehicle_owners.from_date = (select MAX(vehicle_owners1.from_date) from vehicle_owners as vehicle_owners1 where vehicle_owners1.vehicle_id = job_orders.vehicle_id)');
+				})
+				->join('customers', 'customers.id', 'vehicle_owners.customer_id')
+				->join('models', 'models.id', 'vehicles.model_id')
 				->join('configs', 'configs.id', 'job_cards.status_id')
-				->where('users.user_type_id', 1)
-				->where('employees.id', $request->employee_id)
-				->groupBy('job_cards.job_card_number')
+				->where('repair_order_mechanics.mechanic_id', $request->user_id)
+				->groupBy('job_order_repair_orders.job_order_id')
 				->get();
 
 			return response()->json([
