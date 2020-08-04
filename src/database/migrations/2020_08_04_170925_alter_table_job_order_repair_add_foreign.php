d41b62279587a5b74e18892c9a6d55105c5e9760
Parthi <parthiban@uitouxsolutions.com>
2020-08-04 17:42:47 +0530

    migration files

---
+++ src/database/migrations/2020_08_04_170925_alter_table_job_order_repair_add_foreign.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableJobOrderRepairAddForeign extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::table('job_order_repair_orders', function (Blueprint $table) {
+			$table->foreign('estimate_order_id')->references('id')->on('job_order_estimates')->onDelete('SET NULL')->onUpdate('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::table('job_order_repair_orders', function (Blueprint $table) {
+			$table->dropForeign('job_order_repair_orders_estimate_order_id_foreign');
+		});
+	}
+}
