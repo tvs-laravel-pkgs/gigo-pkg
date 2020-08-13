a0e891af8fe62943bb5aab016e4b366458b7ea80
arun-uitoux <arunkumar@uitoux.in>
2020-08-13 16:04:20 +0530

    removing aggregates, sub aggregates, complaints-subaggregate id migrations

--- src/database/migrations/2020_08_12_153532_add_sub_aggregates_col_complaints_table.php
+++ src/database/migrations/2020_08_12_153532_add_sub_aggregates_col_complaints_table.php
@@ -12,18 +12,21 @@ class AddSubAggregatesColComplaintsTable extends Migration {
 	 */
 	public function up() {
 		Schema::table('complaints', function (Blueprint $table) {
-			DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
-			// $table->dropForeign('complaints_company_id_foreign');
-			$table->dropForeign('complaints_group_id_foreign');
+			/*
+				DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
-			$table->dropUnique('complaints_company_id_group_id_code_unique');
-			$table->dropColumn('group_id');
+				// $table->dropForeign('complaints_company_id_foreign');
+				$table->dropForeign('complaints_group_id_foreign');
 
-			$table->unsignedInteger('sub_aggregate_id')->after('name')->nullable();
-			$table->unique(['company_id', 'sub_aggregate_id', 'code']);
-			$table->foreign('sub_aggregate_id')->references('id')->on('sub_aggregates')->onDelete('CASCADE')->onUpdate('CASCADE');
-			DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+				$table->dropUnique('complaints_company_id_group_id_code_unique');
+				$table->dropColumn('group_id');
+
+				$table->unsignedInteger('sub_aggregate_id')->after('name'); //->nullable();
+				$table->unique(['company_id', 'sub_aggregate_id', 'code']);
+				$table->foreign('sub_aggregate_id')->references('id')->on('sub_aggregates')->onDelete('CASCADE')->onUpdate('CASCADE');
+				DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+			*/
 		});
 	}
 
@@ -34,18 +37,20 @@ class AddSubAggregatesColComplaintsTable extends Migration {
 	 */
 	public function down() {
 		Schema::table('complaints', function (Blueprint $table) {
-			DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+			/*
+				DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
-			$table->dropUnique('complaints_company_id_sub_aggregate_id_code_unique');
-			$table->dropForeign('complaints_sub_aggregate_id_foreign');
-			$table->dropColumn('sub_aggregate_id');
+				$table->dropUnique('complaints_company_id_sub_aggregate_id_code_unique');
+				$table->dropForeign('complaints_sub_aggregate_id_foreign');
+				$table->dropColumn('sub_aggregate_id');
 
-			$table->unsignedInteger('group_id')->after('name')->nullable();
-			$table->foreign("group_id")->references("id")->on("complaint_groups")->onDelete("CASCADE")->onUpdate("CASCADE");
+				$table->unsignedInteger('group_id')->after('name'); // ->nullable();
+				$table->foreign("group_id")->references("id")->on("complaint_groups")->onDelete("CASCADE")->onUpdate("CASCADE");
 
-			$table->unique(['company_id', 'group_id', 'code']);
+				$table->unique(['company_id', 'group_id', 'code']);
 
-			DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+				DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+			*/
 
 		});
 	}
