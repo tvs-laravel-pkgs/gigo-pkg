5cf023b8daf5a17d09265c503aabba42ebf08366
Parthi <parthiban@uitouxsolutions.com>
2020-08-21 15:49:46 +0530

    Update Migration

--- src/database/migrations/2020_08_21_151427_add_table_gigo_invoice_item.php
+++ src/database/migrations/2020_08_21_151427_add_table_gigo_invoice_item.php
@@ -16,7 +16,6 @@ class AddTableGigoInvoiceItem extends Migration {
 			$table->unsignedInteger('invoice_id');
 			$table->unsignedInteger('entity_type_id');
 			$table->unsignedInteger('entity_id');
-			$table->unsignedInteger('grn_number_id');
 			$table->unsignedDecimal('qty', 8, 2);
 			$table->unsignedDecimal('mrp', 12, 2);
 			$table->unsignedDecimal('amount', 12, 2);
@@ -28,7 +27,6 @@ class AddTableGigoInvoiceItem extends Migration {
 			$table->softDeletes();
 			$table->foreign('invoice_id')->references('id')->on('gigo_invoices')->onDelete('cascade')->onUpdate('cascade');
 			$table->foreign('entity_type_id')->references('id')->on('configs')->onDelete('cascade')->onUpdate('cascade');
-			$table->foreign('grn_number_id')->references('id')->on('parts_grn_details')->onDelete('cascade')->onUpdate('cascade');
 			$table->foreign('status_id')->references('id')->on('configs')->onDelete('cascade')->onUpdate('cascade');
 			$table->foreign('created_by_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
 			$table->foreign('updated_by_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
