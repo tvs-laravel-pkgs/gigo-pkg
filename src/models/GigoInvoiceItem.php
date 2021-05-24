45fdb1f61d2d7a4f3f0977007705cdbd8c13cceb
Parthiban <parthiban@uitouxsolutions.com>
2021-05-24 20:11:50 +0530

    Relation add

--- src/models/GigoInvoiceItem.php
+++ src/models/GigoInvoiceItem.php
@@ -4,6 +4,14 @@ namespace Abs\GigoPkg;
 
 use App\BaseModel;
 
-class GigoInvoiceItem extends BaseModel {
-	//
+class GigoInvoiceItem extends BaseModel
+{
+
+    protected $table = 'gigo_invoice_items';
+    public $timestamps = false;
+
+    public function taxes()
+    {
+        return $this->belongsToMany('App\Tax', 'gigo_invoice_item_tax', 'invoice_item_id', 'tax_id')->withPivot(['percentage', 'amount']);
+    }
 }
