8b163535743994afb3f036f1696ca6077bf71b0b
Parthiban <parthiban@uitouxsolutions.com>
2021-12-17 19:13:01 +0530

    Updates

--- src/controllers/Api/GateInController.php
+++ src/controllers/Api/GateInController.php
@@ -357,7 +357,7 @@ class GateInController extends Controller
             }
 
             //CHECK VEHICLE PREVIOUS JOBCARD STATUS
-            $previous_job_order = JobOrder::join('outlets','outlets.id','job_orders.outlet_id')->where('job_orders.vehicle_id', $vehicle->id)->orderBy('job_orders.id', 'DESC')->select('outlets.ax_name','outlets.code','job_orders.status_id')->first();
+            $previous_job_order = JobOrder::join('outlets','outlets.id','job_orders.outlet_id')->where('job_orders.vehicle_id', $vehicle->id)->where('job_orders.company_id', Auth::user()->company_id)->orderBy('job_orders.id', 'DESC')->select('outlets.ax_name','outlets.code','job_orders.status_id')->first();
 
             if ($previous_job_order) {
                 if ($previous_job_order->status_id != 8470 && $previous_job_order->status_id != 8476 && $previous_job_order->status_id != 8467 && $previous_job_order->status_id != 8468 && $previous_job_order->status_id != '') {
